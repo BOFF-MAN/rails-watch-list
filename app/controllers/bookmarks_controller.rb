2d22ee519f319062453e0ea02d82b24ca51d162e
David Bradford <db5382@gmail.com>
2022-05-03 20:29:28 +0100

    pushing all code from watch list day 1, only backend done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,33 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(params[:bookmark][:movie_id].to_i)
+    @list = List.find(params[:list_id])
+    @bookmark.movie = @movie
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    # @list = List.find(params[:list_id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
